435d3b0cfd2032d91cd1accb93c62024b5ba20a3
hackerek12138 <162291604+hackerek12138@users.noreply.github.com>
2024-11-11 18:40:05 +0800

    Update bilibiliCrawler-byhacker12138.py

--- bilibiliCrawler-byhacker12138.py
+++ bilibiliCrawler-byhacker12138.py
@@ -12,7 +12,7 @@ import json
 #urlinput = (input("请输入网址："))
 
 
-cookie = "buvid3=86AB49DE-7F6E-0DE7-18AA-4F50638DAA9231847infoc; b_nut=1731207231; bsource=search_bing; _uuid=E919721C-310D1-61AD-ED5E-7105D344FDDD1032850infoc; buvid4=EDB3CED5-35B9-E4BC-366D-4036DADA38D632545-024111002-m4kjDELoiufSqb%2BsZ%2F20fQ%3D%3D; enable_web_push=DISABLE; buvid_fp=d1e13890a19ef5a48019142b235a7fd9; SESSDATA=0e2b0663%2C1746759248%2C75d58%2Ab1CjAo3jblSK69Qwhuz5BcfIHEjBDEoOZfEafNuwUkX0AsgOm3gSOEu3QqQeVjR1z7FZUSVlFYSXdxRU1UNzFIUlpMWV9ONmYtTlN3a1JCbE5BQ01qS1hKLUM4Z09pQS1VZzNhUVNENm1fNUZsOERoZmJrbm5pMDh6dnFTT0ZvZWpZRHFkdjVwdzhnIIEC; bili_jct=8341c5e4e69c1b1e655df3e14c770f79; DedeUserID=3546558857480758; DedeUserID__ckMd5=d52ec064589c37f3; CURRENT_FNVAL=4048; sid=7l2wjl00; rpdid=|(u)YlJ|lY~|0J'u~Juk|~J|Y; header_theme_version=CLOSE; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzE0ODU1MjIsImlhdCI6MTczMTIyNjI2MiwicGx0IjotMX0.h6ZPv7YK9kqMTbkAv-bABKJrvnRIZC7L3OeR99VN_A8; bili_ticket_expires=1731485462; bp_t_offset_3546558857480758=998098533565333504; b_lsid=BB110BADB_1931548F11E; home_feed_column=4; browser_resolution=1177-962"
+cookie = "yourcookie"
 
 #前置条件，包括待爬取网址url和head
 url = "https://www.bilibili.com/"
